4870e26b8dc5311da695da99ffdc8c7b0dd22aa9
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-02-24 17:59:43 +0530

    [Dipesh] modify RetrieveAllCodeAdded

--- ConnectionTest/Program.cs
+++ ConnectionTest/Program.cs
@@ -12,11 +12,11 @@ namespace ConnectionTest
             int option;
             do
             {
-                Console.WriteLine("1) Insert Record");
-                Console.WriteLine("2) Read All records");
-                Console.WriteLine("3) Update Record");
-                Console.WriteLine("4) Delete Record");
-                Console.WriteLine("5) Exit");
+                Console.WriteLine("1)Insert Record");
+                Console.WriteLine("2)Read All records");
+                Console.WriteLine("3)Update Record");
+                Console.WriteLine("4)Delete Record");
+                Console.WriteLine("5)Exit");
                 Console.WriteLine("Enter Choice");
                 option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
